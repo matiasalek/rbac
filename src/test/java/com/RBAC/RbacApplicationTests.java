847a9bd92ea959d3503988a8c87ed242252deb92
Matias <matias.aleksandrowicz@gmail.com>
2025-05-15 23:40:54 -0300

    First commit

---
+++ src/test/java/com/RBAC/RbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RBAC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
