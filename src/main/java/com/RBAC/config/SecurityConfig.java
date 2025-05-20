7d23995601f290c48131bf93cc4a9b496625691b
Matias <matias.aleksandrowicz@gmail.com>
2025-05-20 16:32:00 -0300

    Added and explored about Spring Security login and CSRF

---
+++ src/main/java/com/RBAC/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.RBAC.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Disabled csrf, enable auth for any request and enable login form
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .formLogin(form -> form.loginPage("/login"))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
