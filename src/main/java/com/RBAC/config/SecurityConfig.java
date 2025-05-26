928c83041db0d1a2d8b2e0290a86306201eed6c5
Matias <matias.aleksandrowicz@gmail.com>
2025-05-25 23:35:37 -0300

    Spring Boot Security learning

--- src/main/java/com/RBAC/config/SecurityConfig.java
+++ src/main/java/com/RBAC/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +22,10 @@ public class SecurityConfig {
                 .formLogin(form -> form.loginPage("/login"))
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+       return new InMemoryUserDetailsManager();
     }
 }
